ff951ca773fa0f2e0a6004ce9722c55c5bb153c1
Bilaal <bilaalsadiq@gmail.com>
2024-11-01 15:43:09 +0000

    Add project files.

---
+++ cmap-timesheet-system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cmap_timesheet_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
